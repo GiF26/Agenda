2c609dd57df65d78c48537157949e86db1673dd6
Giovanna Ferreira <83253307+GiF26@users.noreply.github.com>
2026-06-18 06:00:42 -0300

    Initial commit

---
+++ app/src/main/java/alura/com/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package alura.com.agenda.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import alura.com.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
---
+++ app/src/main/java/alura/com/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,30 @@
+package alura.com.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import alura.com.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle("Lista de alunos");
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Giovanna", "Fran", "José", "Felipe", "Maria", "Júlia"));
+        ListView alunosList = findViewById(R.id.activity_lista_alunos_listview);
+        alunosList.setAdapter( new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos) );
+
+    }
+}
+
